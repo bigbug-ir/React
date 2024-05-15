e90e0a11318eef063a28ce2d316564d25e7b024b
AmirHosein <amirhosseinbakhtiyari.program@gmail.com>
2024-05-15 21:33:33 +0330

    I create a simple timer whit components #part8

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const tick =()=>{
-  const element=(
-    <div>
-      <h1>hello world</h1>
+class Timer extends React.Component {
+  render(){
+    return(
       <h2>it is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
+    )
+  }
+}
+class Hello extends React.Component {
+  render(){
+    return(
+      <h1>Hello World</h1>
+    )
+  }
+}
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <Hello/>
+        <Timer/>
+      </div>
+    );
+    }
+}
 
-ReactDOM.render( element, document.getElementById('root'));
-
-};
-setInterval(() => {
+// let elem = new App();
+const tick= ()=>{
+  ReactDOM.render(<App/>,document.getElementById('root'));
+}
+setInterval(() =>{ 
   tick();
 }, 1000);
